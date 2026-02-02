c707c34184a9ac42e56bb0ee9973d93e28b2d150
Jon Baarsch <jdbaarsch@gmail.com>
2026-02-02 07:36:52 -0600

    Initial Push

---
+++ src/main/java/com/baarsch_bytes/Licenser.java
@@ -0,0 +1,17 @@
+package com.baarsch_bytes;
+
+public class Licenser {
+
+
+    boolean issueLicense(double trainingHours, boolean testPassed) {
+
+        if (trainingHours >= 40 && testPassed)
+            return true;
+        return false;
+
+    }
+
+
+
+
+}
---
+++ src/test/java/com/baarsch_bytes/AppTest.java
@@ -0,0 +1,27 @@
+package com.baarsch_bytes;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void hoursUnder40() {
+
+    }
+
+
+
+}
