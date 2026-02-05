3bbc98e517e25f9ab2f866c6b92422d24334160d
Jon Baarsch <jdbaarsch@gmail.com>
2026-02-05 11:37:16 -0600

    Add Lab 3 and inRange code

---
+++ src/test/java/com/baarsch_bytes/MathUtilsTest.java
@@ -0,0 +1,17 @@
+package com.baarsch_bytes;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class MathUtilsTest {
+
+
+
+
+
+}
