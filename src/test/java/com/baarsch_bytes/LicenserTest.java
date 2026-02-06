dc31fb842eeab3171eb77fdbbb51c384d8b5070e
Jon Baarsch <jdbaarsch@gmail.com>
2026-02-06 08:38:22 -0600

    Add Lab 3 and inRange code

--- src/test/java/com/baarsch_bytes/LicenserTest.java
+++ src/test/java/com/baarsch_bytes/LicenserTest.java
@@ -6,23 +6,37 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 
 public class LicenserTest {
 
     private Licenser licenser;
+    public final String[] testCases1 =
+            {       "39.9, true, false",    // trainingHours, testPassed, expectedResult
+                    "40.0, true, true",
+                    "50, false, false"
+            };
+    public static String testCase1 = "39.9, true, false";
+    public static String testCase2 = "40.0, true, true";
+    public static String testCase3 = "50, false, false";
+
 
     @BeforeEach
     void testSetup(){
         licenser = new Licenser();
     }
 
+    //@CsvSource(TestCases.testCases1)
+
     @ParameterizedTest
-    @CsvSource({
+    /*@CsvSource({
             "39.9, true, false",    // trainingHours, testPassed, expectedResult
             "40.0, true, true",
             "50, false, false"
-    })
+    })*/
+    @CsvFileSource(resources="/Book1.csv")
+    //@CsvSource("Book1.csv")
     void testTrainingHoursAndTestPassed(double trainingHours, boolean testPassed, boolean expectedResult) {
         assertEquals(expectedResult,
                 licenser.issueLicense(trainingHours, testPassed),
