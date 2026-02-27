9d35a9d8f81a85a52e889a271d6b12ad454c9859
Jon Baarsch <jdbaarsch@gmail.com>
2026-02-27 08:33:39 -0600

    Add giveDiscount example from Textbook with Old Version and Revised Version also added converter for Long.MIN_VALUE and Long.MAX_VALUE

---
+++ src/test/java/com/baarsch_bytes/OnlineSalesTest.java
@@ -0,0 +1,25 @@
+package com.baarsch_bytes;
+
+import com.baarsch_bytes.Converters.LongConstantConverter;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.converter.ConvertWith;
+import org.junit.jupiter.params.converter.SimpleArgumentConverter;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import com.baarsch_bytes.OnlineSalesOld.Status;
+import com.baarsch_bytes.OnlineSalesOld.Status.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class OnlineSalesTest {
+
+    @ParameterizedTest
+    @CsvFileSource(resources="/OnlineSalesDiscount_tests.csv", numLinesToSkip = 1)
+    public void testGiveDiscount(String id, @ConvertWith(LongConstantConverter.class) long bonusPoints, boolean goldCustomer,
+                                 OnlineSalesOld.Status expected) {
+        assertEquals(expected,
+               // OnlineSalesOld.giveDiscount(bonusPoints, goldCustomer) );
+                OnlineSalesRevised.giveDiscount(bonusPoints, goldCustomer) );
+    }
+
+
+}
