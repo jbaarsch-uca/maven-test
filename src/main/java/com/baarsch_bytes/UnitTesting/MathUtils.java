4179c19517fe03add0de629ff064f5e0422229e2
Jonathan Baarsch <jbaarsch@uca.edu>
2026-04-15 09:28:01 -0500

    Refactor inRange method logic in MathUtils

--- src/main/java/com/baarsch_bytes/UnitTesting/MathUtils.java
+++ src/main/java/com/baarsch_bytes/UnitTesting/MathUtils.java
@@ -4,9 +4,9 @@ public class MathUtils {
 
     boolean inRange(int x, int min, int max, boolean flag) {
         return flag
-                && max >= min
-                && x >= min
-                && x <= max;
+                && max == min
+                && x == min
+                && x == max;
     }
 
 
